034a240bf04884ff110f7f0be13bb92cc26a9166
Olga Bernal <obersan6@gmail.com>
2024-08-07 21:21:01 -0400

    Part 2 - No Bootstrap used yet

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 """Blogly application."""
 
 # from flask import Flask, request, render_template, redirect, url_for, session, flash
-from flask import Flask, request, render_template, redirect, url_for
+from flask import Flask, request, render_template, redirect, url_for, flash
 from flask_debugtoolbar import DebugToolbarExtension
-from models import db, connect_db, User
+from models import db, connect_db, User, Post
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "chocolate"
@@ -53,7 +53,8 @@ def add_user():
 def user(user_id):
     """Show info of a user, show button to their edit page and delete user"""
     user = User.query.get_or_404(user_id)
-    return render_template('user_details.html', user=user)
+    posts = Post.query.filter_by(user_id=user_id).all()
+    return render_template('user_details.html', user=user, posts=posts)
 
 @app.route('/users/<int:user_id>/edit', methods=['GET', 'POST'])
 def edit_user(user_id):
@@ -75,16 +76,73 @@ def delete_user(user_id):
     db.session.commit()
     return redirect(url_for('list_users'))
 
+# GET all posts
+@app.route('/posts')
+def show_posts():
+    """Show all posts"""
+    posts = Post.query.all()
+    return render_template('posts.html', posts=posts)
+
+# GET /users/[user-id]/posts/new --> Show form to add a post for that user.
+@app.route('/users/<int:user_id>/posts/new')
+def add_post(user_id):
+    """Show form to add a post for that user."""
+    user = User.query.get_or_404(user_id)
+    return render_template('add_post_form.html', user=user)
+
+# POST /users/[user-id]/posts/new --> Handle add form; add post and redirect to the user detail page.
+@app.route('/users/<int:user_id>/posts/new', methods=['POST'])
+def submit_post(user_id):
+    """Handle add form; add post and redirect to the user detail page"""
+    user = User.query.get_or_404(user_id)
+    
+    title = request.form['title']
+    content = request.form['content']
+
+    new_post = Post(title=title, content=content, user_id=user.id)
+    db.session.add(new_post)
+    db.session.commit()
+
+    return redirect(url_for('user', user_id=user.id))
+
+# GET /posts/[post-id] --> Show a post. Show buttons to edit and delete the post.
+@app.route('/posts/<int:post_id>')  
+def show_post(post_id):
+    """Show a post, buttons to edit and delete the post"""
+    post = Post.query.get_or_404(post_id)
+    return render_template('post.html', post=post)
+
+# GET route--> /posts/[post-id]/edit --> Show form to edit a post, and to cancel (back to user page).
+@app.route('/posts/<int:post_id>/edit')
+def show_edit_post_form(post_id):
+    """Show form to edit a post, and to cancel (back to user page)"""
+    post = Post.query.get_or_404(post_id)
+    return render_template('edit_post.html', post=post)
+
+# POST route --> /posts/[post-id]/edit --> Handle editing of a post. Redirect back to the post view.
+@app.route('/posts/<int:post_id>/edit', methods=['POST'])
+def edit_post(post_id):
+    """Handle editing of a post. Redirect back to the show_post"""
+    post = Post.query.get_or_404(post_id)
+
+    post.title = request.form['title']
+    post.content = request.form['content']
+    db.session.commit()
+
+    return redirect(url_for('show_post', post_id=post.id))
+
+# POST route /posts/[post-id]/delete --> Delete the post.
+@app.route('/posts/<int:post_id>/delete', methods=['POST'])
+def delete_post(post_id):
+    """Delete a post. Redirect to the list of posts"""
+    post = Post.query.get_or_404(post_id)
+    user_id = post.user_id # Get the user ID before deleting the post
+    db.session.delete(post)
+    db.session.commit()
+    return redirect(url_for('user', user_id=user_id)) # Redirect to the user's detail page
 
 
 
-# MAKE ROUTES FOR:**
-
-# **GET */users/[user-id]/edit :*** Show the edit page for a user. Have a cancel button that returns to the detail page for a user, and a save button that updates the user.
-
-# **POST */users/[user-id]/edit :***Process the edit form, returning the user to the ***/users*** page.
-
-# **POST */users/[user-id]/delete :*** Delete the user.
 
 
 
--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 """Models for Blogly."""
 
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import func
 
 db = SQLAlchemy()
 
@@ -20,22 +21,31 @@ class User(db.Model):
         last_name = db.Column(db.String(50), nullable = False)
         image_url = db.Column(db.String(200), nullable = True)
 
+class Post(db.Model):
+      __tablename__ = 'posts'
+
+      id = db.Column(db.Integer, primary_key = True, autoincrement = True)
+      title = db.Column(db.String(150), nullable = False)
+      content = db.Column(db.String(500), nullable = False)
+      created_at = db.Column(db.DateTime, default = func.now())  # Automatically set to current date and time)
+
+      # foreign key
+      user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+      # Create relationship with User
+      user = db.relationship('User', backref=db.backref('posts', lazy=True))
 
 
 
-# class Pet(db.Model):
-#     __tablename__ = 'pets'
 
-#     # Columns
-#     id = db.Column(db.Integer, primary_key = True, autoincrement = True)
-#     #"autoincrement" is like "SERIAL" in SQL
-#     name = db.Column(db.String (50), nullable=False, unique=True)  --> #A string with a max of 50 char., 
-#                                                                        # cannot be null, must be unique.
+# Next, add another model, for blog posts (call it ***Post***).
 
-#     species = db.Column(db.String (30), nullable=True) --> #It can be NULL
+# Post should have an:
 
-#     hunger = db.Column(db.Integer, nullable=False, default=20)
-
+# - ***id***, like for ***User***
+# - ***title***
+# - ***content***
+# - ***created_at*** a date+time that should automatically default to the when the post is created
+# - a foreign key to the ***User*** table
 
 
     
